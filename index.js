c1e0cb64996a3c01a5ec4c51f27ddd8a300cde8c
LuckyYoila <luckyyoila626@gmail.com>
2023-06-08 15:41:34 +0100

    added database configurations

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require("express");
-const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+const mysql = require("mysql");
 const app = express();
 
+dotenv.config();
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+})
+
 app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/static"));
 
 app.get("/", (req, res) => {
@@ -14,16 +24,6 @@ app.get("/about", (req, res) => {
   res.render("about");
 });
 
-// app.post('/', (req, res) => {
-//     console.log(req.body)
-
-//     res.redirect("/about")
-// })
-
-// app.get("/about", (req, res) => {
-
-//     res.sendFile(__dirname + "/html/about.html");
-// })
 
 const port = 3000;
 app.listen(port, () => console.log("Your app is listening on port " + port));
