c68dc06de844c329d8505c93e15a59b5c7dc9f68
LuckyYoila <luckyyoila626@gmail.com>
2023-06-09 15:22:10 +0100

    added database query to create users, created README file

---
+++ README.md
@@ -0,0 +1,22 @@
+# How to use this application
+
+- Clone the repository
+- cd into the directory and install all the dependencies using
+  ```bash
+      cd expressclass2
+      npm install
+  ```
+- Create a .env file and set the environment varibles as follows:
+
+  ```bash
+
+      DB_HOST = "your-db-host"
+      DB_USER = "your-db-user"
+      DB_NAME = "your_db_name"
+      DB_PASSWORD = "your_db_password"
+  ```
+
+- to run the developmentn server run the following command:
+  ```bash
+      npm run dev
+  ```
--- index.js
+++ index.js
@@ -1,29 +1,58 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const mysql = require("mysql");
+const mysql = require("mysql2");
 const app = express();
 
 dotenv.config();
 
-const connection = mysql.createConnection({
+const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
-})
+  database: process.env.DB_NAME,
+});
+
+db.connect((err) => {
+  if (err) throw err;
+  console.log("Database connected");
+});
 
 app.set("view engine", "ejs");
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/static"));
 
-app.get("/", (req, res) => {
-  res.render("index", { name: "lucky",surname: "yoila", age: 20});
+app.get("/", async (req, res) => {
+ db.query("SELECT * FROM users", async (err, result) => {
+    if (err) throw err;
+    // console.log("Users: ", result);
+      
+    res.render("index", {
+      users: result,
+    });
+  });
+});
+
+// where the submitted information is posted to from the form
+app.post("/register", (req, res) => {
+  const { firstname, surname, gender, email, password } = req.body;
+  db.query(
+    `insert into users(first_name, last_name, gender, email, password) values("${firstname}", "${surname}", "${gender}", "${email}", "${password}")`,
+    (err) => {
+      if (err) throw err;
+    }
+  );
+
+  res.redirect("/");
 });
 
 app.get("/about", (req, res) => {
   res.render("about");
 });
 
-
 const port = 3000;
 app.listen(port, () => console.log("Your app is listening on port " + port));
+
+module.exports = {
+  db,
+};
